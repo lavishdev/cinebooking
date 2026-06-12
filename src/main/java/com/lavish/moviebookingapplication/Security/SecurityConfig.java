ab7f822948aa9d5a8a747be3a2b5b8a144330f39
lavishdev <saini.lavish2007@gmail.com>
2026-06-12 13:44:41 +0530

    Update securityconfig for netlify

--- src/main/java/com/lavish/moviebookingapplication/Security/SecurityConfig.java
+++ src/main/java/com/lavish/moviebookingapplication/Security/SecurityConfig.java
@@ -88,9 +88,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(frontendUrl));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:*", "https://*.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
